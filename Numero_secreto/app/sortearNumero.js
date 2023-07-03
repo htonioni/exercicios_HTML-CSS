a86351837d55fc5397482dc4e19815e397617d7d
htonioni <htonioni@outlook.com>
2023-07-03 08:27:48 -0300

    Update main files

---
+++ Numero_secreto/app/sortearNumero.js
@@ -0,0 +1,13 @@
+const menorValor = 1;
+const maiorValor = 100;
+const numeroSecreto = gerarNumeroAleatorio();
+
+function gerarNumeroAleatorio() {
+    return parseInt(Math.random() * maiorValor + 1)
+};
+console.log("Numero secreto eh:",numeroSecreto);
+
+const elementoMenorValor = document.getElementById("menor-valor");
+const elementoMaiorValor = document.getElementById("maior-valor");
+elementoMenorValor.innerHTML = menorValor;
+elementoMaiorValor.innerHTML = maiorValor;
