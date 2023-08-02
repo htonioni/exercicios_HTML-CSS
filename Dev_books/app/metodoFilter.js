e04591b823eeb95ec5ac2f02c466b725b06a18f2
htonioni <htonioni@outlook.com>
2023-08-02 11:45:42 -0300

    Added availble books btn function

--- Dev_books/app/metodoFilter.js
+++ Dev_books/app/metodoFilter.js
@@ -1,10 +1,10 @@
 const botoes = document.querySelectorAll('.btn')
- botoes.forEach(btn => btn.addEventListener('click', filtrarLivros))
+botoes.forEach(btn => btn.addEventListener('click', filtrarLivros))
 
 function filtrarLivros() {   
     const elementoBtn = document.getElementById(this.id)
     const categoria = elementoBtn.value
-    console.log(categoria);
-    let livrosFiltrados = livros.filter(livro => livro.categoria == categoria)
+    let livrosFiltrados = categoria == 'disponivel' ? livros.filter(livro => livro.quantidade > 0) : livros.filter(livro => livro.categoria == categoria)
+
     exibirOsLivrosNaTela(livrosFiltrados)
 };
