8f8714e331ef45d05dd68d395811ad352e844d58
htonioni <htonioni@outlook.com>
2023-08-02 11:55:52 -0300

    Extracted functions to global

--- Dev_books/app/metodoFilter.js
+++ Dev_books/app/metodoFilter.js
@@ -4,7 +4,25 @@ botoes.forEach(btn => btn.addEventListener('click', filtrarLivros))
 function filtrarLivros() {   
     const elementoBtn = document.getElementById(this.id)
     const categoria = elementoBtn.value
-    let livrosFiltrados = categoria == 'disponivel' ? livros.filter(livro => livro.quantidade > 0) : livros.filter(livro => livro.categoria == categoria)
+    let livrosFiltrados = categoria == 'disponivel' ? filtrarPorDisponibilidade() : filtrarPorCategoria(categoria)
 
     exibirOsLivrosNaTela(livrosFiltrados)
+    if (categoria == 'disponivel') {
+        exibirValorTotalLivrosNaTela()
+    }
 };
+
+function filtrarPorCategoria(categoria) {
+    return livros.filter(livro => livro.categoria == categoria)
+}
+
+function filtrarPorDisponibilidade() {
+    return livros.filter(livro => livro.quantidade > 0)
+}
+
+function exibirValorTotalLivrosNaTela() {
+    elementoComValorTotalDeLivrosDisponiveis.innerHTML = `    
+    <div class="livros__disponiveis">
+    <p>Todos os livros disponíveis por R$ <span id="valor"></span></p>
+  </div>`
+}
