5cb606ccaa7259a93565b1a08003280de36a099b
htonioni <htonioni@outlook.com>
2023-08-16 08:27:08 -0300

    Added link to next page

--- Your_bank/app/script.js
+++ Your_bank/app/script.js
@@ -2,7 +2,22 @@ import ehUmCPF from "./validaCPF.js";
 import ehMaiorDeIdade from "./valida-idade.js";
 
 const camposDoFormulario = document.querySelectorAll("[required]");
-console.log(Date());
+const formulario = document.querySelector("[data-formulario]");
+
+formulario.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const listaRespostas = {
+        "nome": e.target.elements["nome"].value,
+        "email": e.target.elements["email"].value,
+        "rg": e.target.elements["rg"].value,
+        "cpf": e.target.elements["cpf"].value,
+        "aniversario": e.target.elements["aniversario"].value,
+    }
+
+    localStorage.setItem("cadastro", JSON.stringify(listaRespostas));
+
+    window.location.href = "./abrir-conta-form-2.html"
+})
 
 camposDoFormulario.forEach((barrinhaCampo) => {
     // Blur -> quando seleciona fora de uma "barrinha"
